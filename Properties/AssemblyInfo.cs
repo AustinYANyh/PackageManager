5b71f063b29802800aa6e6b31ac833a477376516
AustinYanyh <1241528539@qq.com>
2026-04-21 14:48:27 +0800

    update: Bump version to 3.1.0.0 and update copyright year to 2026

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PackageManager")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.5")]
+[assembly: AssemblyVersion("3.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
